8405271b5b7680c293da432b1b4c82ed70bd97d0
kokoosso <raphael.rodbelo@gmail.com>
2021-04-26 11:00:26 -0300

    dia 4 feito

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,35 @@ import { Player } from '../components/Player/index';
 
 import '../styles/global.scss';
 import styles from '../styles/app.module.scss';
+import { PlayerContext } from '../contexts/PlayerContext';
+import { useState } from 'react';
 
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false)
+  function playEpisode(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+  function togglePlay() {
+    setIsPlaying(!isPlaying)
+  }
+  function setPlayingState(state: boolean) {
+    setPlayingState(state)
+  }
   return (
-    <div className={styles.appWrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
-
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, playEpisode, isPlaying, togglePlay, setPlayingState }}>
+      <div className={styles.appWrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
